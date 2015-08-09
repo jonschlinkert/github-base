0c52e6a1e4b759743e3b556f7db7a9a3df457a06
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-09 11:01:22 -0400

    refactor

--- test/test.js
+++ test/test.js
@@ -13,6 +13,32 @@ var fake_user = {
   PASSWORD: 'fakeuser123'
 };
 
+describe('static methods', function () {
+  it('should inherit GitHub with `.extend`', function () {
+    function Foo() {
+      GitHub.call(this);
+    }
+    GitHub.extend(Foo);
+    Foo.prototype.should.have.property('get');
+    Foo.prototype.should.have.property('put');
+  });
+
+  it('should add prototype methods with delegate:', function () {
+    function Foo() {
+      GitHub.call(this);
+    }
+    GitHub.extend(Foo);
+    GitHub.delegate({
+      bar: function () {}
+    });
+    Foo.delegate({
+      baz: function () {}
+    });
+    Foo.prototype.should.have.property('bar');
+    Foo.prototype.should.have.property('baz');
+  });
+});
+
 describe('GitHub API', function () {
   beforeEach(function () {
     this.timeout(10000);
@@ -23,155 +49,43 @@ describe('GitHub API', function () {
     });
   });
 
-  it('should `get` a response', function (done) {
-    this.slow(500);
+  describe('GET', function () {
+    it('should get user orgs:', function (done) {
+      this.slow(500);
 
-    github.get('/user/orgs', function (err, res) {
-      if (err) return done(err);
+      github.get('/user/orgs', function (err, res) {
+        if (err) return done(err);
 
-      assert.equal(Array.isArray(res), true);
-      assert.deepEqual(utils.arrayKeys(res), [
-        'login',
-        'id',
-        'url',
-        'repos_url',
-        'events_url',
-        'members_url',
-        'public_members_url',
-        'avatar_url',
-        'description'
-      ]);
-      done();
+        assert.equal(Array.isArray(res), true);
+        assert.deepEqual(utils.arrayKeys(res), [
+          'login',
+          'id',
+          'url',
+          'repos_url',
+          'events_url',
+          'members_url',
+          'public_members_url',
+          'avatar_url',
+          'description'
+        ]);
+        done();
+      });
     });
   });
 
-  // it('should return github.gists', function (done) {
-  //   this.slow(500);
-  //   github.gists(function (err, res) {
-  //     if (err) return done(err);
+  describe('PUT', function () {
+    it('should follow a user', function (done) {
+      this.slow(500);
 
-  //     assert.equal(Array.isArray(res), true);
-  //     assert.deepEqual(utils.arrayKeys(res), [
-  //       'url',
-  //       'forks_url',
-  //       'commits_url',
-  //       'id',
-  //       'git_pull_url',
-  //       'git_push_url',
-  //       'html_url',
-  //       'files',
-  //       'public',
-  //       'created_at',
-  //       'updated_at',
-  //       'description',
-  //       'comments',
-  //       'user',
-  //       'comments_url',
-  //       'owner'
-  //     ]);
-  //     done();
-  //   });
-  // });
+      github.put('/user/following/jonschlinkert', function(err, res) {
+        if (err) return done(err);
 
-  // it('should return github.notifications', function (done) {
-  //   this.slow(500);
-  //   github.notifications(function (err, res) {
-  //     if (err) return done(err);
-
-  //     assert.equal(Array.isArray(res), true);
-  //     assert.deepEqual(utils.arrayKeys(res), [
-  //       'id',
-  //       'unread',
-  //       'reason',
-  //       'updated_at',
-  //       'last_read_at',
-  //       'subject',
-  //       'repository',
-  //       'url',
-  //       'subscription_url'
-  //     ]);
-  //     done();
-  //   });
-  // });
-
-  // it('should return github.show', function (done) {
-  //   this.slow(500);
-  //   github.show('doowb', function (err, res) {
-  //     if (err) return done(err);
-  //     assert.equal(typeof res === 'object', true);
-  //     assert.equal(res.hasOwnProperty(('login')), true);
-  //     assert.equal(res.login === 'doowb', true);
-  //     done();
-  //   });
-  // });
-
-  // it('should return github.userRepos', function (done) {
-  //   this.slow(500);
-  //   github.userRepos(fake_github.USERNAME, function (err, res) {
-  //     if (err) return done(err);
-
-  //     assert.equal(Array.isArray(res), true);
-  //     var keys = ['full_name', 'owner', 'html_url', 'description', 'fork'];
-  //     assert.equal(utils.hasKeys(keys, res), true);
-  //     done();
-  //   });
-  // });
-
-  // it('should return github.userGists', function (done) {
-  //   this.slow(500);
-  //   github.userGists(fake_github.USERNAME, function (err, res) {
-  //     if (err) return done(err);
-
-  //     assert.equal(Array.isArray(res), true);
-  //     assert.deepEqual(utils.arrayKeys(res), [
-  //       'url',
-  //       'forks_url',
-  //       'commits_url',
-  //       'id',
-  //       'git_pull_url',
-  //       'git_push_url',
-  //       'html_url',
-  //       'files',
-  //       'public',
-  //       'created_at',
-  //       'updated_at',
-  //       'description',
-  //       'comments',
-  //       'user',
-  //       'comments_url',
-  //       'owner'
-  //     ]);
-  //     done();
-  //   });
-  // });
-
-  // it('should return github.orgRepos', function (done) {
-  //   this.timeout(10000);
-  //   github.orgRepos('upstage', function (err, res) {
-  //     if (err) return done(err);
-
-  //     assert.equal(Array.isArray(res), true);
-  //     var keys = ['id', 'name', 'full_name', 'owner'];
-  //     assert.equal(utils.hasKeys(keys, res), true);
-  //     done();
-  //   });
-  // });
-
-  // it('should return github.follow', function (done) {
-  //   this.slow(500);
-  //   github.follow('gisthub', function (err, res) {
-  //     if (err) return done(err);
-  //     done();
-  //   });
-  // });
-
-  // it('should return github.unfollow', function (done) {
-  //   this.slow(500);
-  //   github.unfollow('gisthub', function (err, res) {
-  //     if (err) return done(err);
-  //     done();
-  //   });
-  // });
+        console.log(arguments)
+        // assert.equal(Array.isArray(res), true);
+        done();
+      });
+    });
+  });
 });
 
 
