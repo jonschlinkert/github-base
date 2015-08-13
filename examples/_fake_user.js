e0d60240470ad69d96dcd1f48b9d757c5897890b
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-13 05:21:47 -0400

    fix opts

--- examples/_fake_user.js
+++ examples/_fake_user.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: 'fa9b3d7cb70c378779443e601bdf6c4bc59c79ad',
+  // TOKEN: 'fa9b3d7cb70c378779443e601bdf6c4bc59c79ad',
   USERNAME: 'fake-user123',
   PASSWORD: 'fakeuser123'
 };
